50ba9b845cafee13e12d2094b87928c2d49170aa
Mike Zarra <mkzarra@Mikes-Air.fios-router.home>
2020-03-20 17:17:47 -0400

    refactors tests and index posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-      user = User.create!(email: "finthehuman@candykingdom.ooo", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Fin", last_name: 'the Human')
-      login_as(user, :scope => :user)
+      @user = User.create!(email: "finthehuman@candykingdom.ooo", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Fin", last_name: 'the Human')
+      login_as(@user, :scope => :user)
   end
 
   describe 'index' do
     before do
       visit posts_path
     end
-    
+
     it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
@@ -20,8 +20,8 @@ describe 'navigate' do
     end
 
     it 'has a list of posts' do
-      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: User.last.id)
-      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: User.last.id)
+      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
       visit posts_path
       expect(page).to have_content(/Post1|Post2/)
     end
